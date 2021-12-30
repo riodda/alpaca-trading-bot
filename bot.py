0dbd51b98ec709eacfd740bebc6fb278726293d8
Dario <riodda@gmail.com>
2021-12-30 18:06:06 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,7 +20,7 @@ import math
 import os
 from config import *
 
-WATB_VERSION = '0.1-b.1'
+WATB_VERSION = '0.1-b.2'
 __version__ = WATB_VERSION
 
 def webhookParse(webhook_data):
@@ -151,7 +151,7 @@ def webhookListen():
             #close code, check if market open then retrive postions find position for symbol check qty then close sell position
             #Retrive Position
             position = rff.alpaca_get_position(json_data['symbol']) 
-            if ((rff.alpaca_check_market_open() == True) and (not(position == False))):
+            if ((rff.alpaca_check_market_open() == True) and (not(position == False)) and (float(unrealized_plpc.unrealized_plpc) > min_pct_for_sell)):
                 order = rff.alpaca_close_position(json_data['symbol'])
                 print(order)
                 rff.writelog("TRADING,CLOSE,"+str(json_data['symbol'])+","+str(position.qty)+","+str(last_price)+","+str(order.id)+","+str(position.unrealized_pl))
@@ -191,3 +191,5 @@ if __name__ == '__main__':
     scheduler.start()
     flaskServer.run(host=str(ip_address),port=webhook_port, debug=True)
     
+
+   
