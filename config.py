ba1faf4dfd91bc7073ce746d0534419bb09b61f8
Dario <riodda@gmail.com>
2021-12-30 18:02:52 +0100

    Update config.py

--- config.py
+++ config.py
@@ -6,10 +6,16 @@ key = "MYKEY"
 secretKey = "MYSECRET"
 #change for live trading
 api_url = "https://paper-api.alpaca.markets"
-log_dir = "C:\#temp"
-payload_dir = "C:\#temp"
+#change for desired log and payload folders
+log_dir = "/var/log/"
+payload_dir = "/root/"
+#public ip address of for fask server
 ip_address = "127.0.0.1"
-day_trade = False
 webhook_port = 80
+#Enable daytrade
+day_trade = False
 min_pct_for_sell = 0
+#delete this keys if dont whant log forwarded to telegram
+telegram_bot_token = 'telegram_token'
+telegram_bot_chatID = 'telegram_chat_id'
 
